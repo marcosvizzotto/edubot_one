9d2397850eb2583cb43bbbc4899d2567c78b4f46
marcosvizzotto <marcosvizzotto@hotmail.com>
2023-12-09 22:30:53 -0300

    feat: Add laser scan

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -32,9 +32,10 @@ def generate_launch_description():
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    #Execute o nó spawner do pacote gazebo_ros. O nome da entidade realmente não importa se você tiver apenas um robô.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'edubot_one'],
                         output='screen')
 
 
